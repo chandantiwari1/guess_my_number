38adfb5e085a7d0263d58d9e0e97a71390679eec
chandan tiwari <developerchandan1@gmail.com>
2022-05-26 04:10:32 +0530

    html and css completed

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent="❤❤chandan pandit";
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".number").textContent=20;
+// document.querySelector(".score").textContent=10;
+
+// document.querySelector(".guess").value=23;
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener('click',function(){
+    console.log(document.querySelector('.guess').value);
+})
